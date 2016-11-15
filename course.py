5e55bbdd7d366301a62b05c25894a53f45a150f0
Apoorv Chitray <apoorv1126@gmail.com>
2016-11-15 14:41:44 +0100

    course.py
    learn python for OSCP

---
+++ course.py
@@ -0,0 +1,12 @@
+import math
+fact=math.factorial(3)
+print `fact`
+print "hello world"
+var1 = input()
+print `var1`
+fact = math.factorial(var1)
+print `fact`
+family = ['mom','dad','sis','bro']
+print `family[2]`
+print `family[-3]`
+list=[0,1,2,3,4,5,6,7,8,9]
