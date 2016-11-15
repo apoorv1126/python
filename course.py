521a4566e3aacc8babf0655787d4f9bfd9f0b453
Apoorv Chitray <apoorv1126@gmail.com>
2016-11-15 15:21:04 +0100

    Update course.py

--- course.py
+++ course.py
@@ -9,4 +9,30 @@ print `fact`
 family = ['mom','dad','sis','bro']
 print `family[2]`
 print `family[-3]`
+
+#slicing example
+
 list=[0,1,2,3,4,5,6,7,8,9]
+print `list[4:8]` #first number inclusive - last not
+print `list[:6]`
+print `list[:]`
+print `list[6:]`
+print `list[2:9:2]`
+print `list[9:1:-2]`
+
+# in keywork example to check the existance of the string or number in the sequence
+print `'mom' in  family`
+
+print `len(list)`
+list.pop(9) #to print the no on the screen and remove it from the list
+
+
+
+
+
+
+
+
+
+
+
